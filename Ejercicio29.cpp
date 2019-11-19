a19bb9921fc9e597d7e5d85a5914e7882424d9d1
Default user <yalejandro.gomez@gmail.com>
2019-11-19 16:52:55 +0000

    Actualizar

--- Ejercicio29.cpp
+++ Ejercicio29.cpp
@@ -9,16 +9,16 @@ double T = 1;
 double Xmin = -1;
 double Xmax = 1;
 int Nt = 100;
-int Nx = 10;
+int Nx = 11;
 double dT = T/Nt;
 double dX = (Xmax-Xmin)/Nx;
 
 int FTCS(double **PSI);
 
 int main(){
-    double **PSI = new double *[Nt];
-    for (int i=0;i<Nt;i++){
-        PSI[i] =new double[Nx];
+    double **PSI = new double *[Nt+1];
+    for (int i=0;i<=Nt;i++){
+        PSI[i] =new double[Nx+1];
     }
     FTCS(PSI);
     
@@ -26,30 +26,44 @@ int main(){
 }
 
 int FTCS(double **PSI){
-    for(int i=0; i<Nt; i++){
-        for(int j=0; j<Nx; j++){
+    for(int i=0; i<=Nt; i++){
+        for(int j=0; j<=Nx; j++){
             if(i==0){
                 PSI[i][j] = 0;
             }
             
-            if(j==0 | j==Nx-1){
+            if(j==0 | j==Nx){
                 PSI[i][j] = 0;
             }
         }
     }
     
-    for(int i=0; i<Nt-1; i++){
-        for(int j=1; j<Nx-1; j++){
+    for(int i=0; i<Nt; i++){
+        for(int j=1; j<Nx; j++){
             PSI[i+1][j] = PSI[i][j]+D*dT*(PSI[i][j+1]-2*PSI[i][j]+PSI[i][j-1])/pow(dX,2)+dT*S;
         }
     }
     
-    for(int i=0; i<Nt; i++){
-        for(int j=0; j<Nx; j++){
-            cout<<PSI[i][j]<<"\t";
+    ofstream outfile;
+    outfile.open("difusion.dat");
+    
+    double time = 0;
+    for(int i=0; i<=Nt; i++){
+        outfile<<time<<"\t";
+        time += dT;
+        for(int j=0; j<=Nx; j++){
+            outfile<<PSI[i][j]<<"\t";
         }
-        cout<<endl;
+        outfile<<endl;
     }
+    
+    double x = -1;
+    for(int j=0; j<=Nx; j++){
+        outfile<<x<<"\t";
+        x += dX;
+    }
+    outfile<<endl;
+    outfile.close();
     
     return 0;
 }
--- Ejercicio29.py
+++ Ejercicio29.py
@@ -6,15 +6,26 @@ difusion = np.loadtxt("difusion.dat")
 
 plt.figure(figsize=(8,4))
 
-x = proyectil[:,0]
-y = proyectil[:,1]
+plt.subplot(1,2,1)
 
-plt.plot(x,y,c='g')
+time = difusion[:,0]
+Psi1 = difusion[:,6]
 
+plt.plot(time,Psi1,c='g')
 plt.grid()
+plt.xlabel('Time (s)')
+plt.ylabel('$\Psi$ ($m^2/s$)')
+plt.title("DIFUSION, x = 0m")
 
-plt.xlabel('X(m)')
-plt.ylabel('Y(m)')
-plt.title("DIFUSION")
+plt.subplot(1,2,2)
+
+x = difusion[101,:]
+Psi2 = difusion[100,:]
+
+plt.plot(x,Psi2,c='g')
+plt.grid()
+plt.xlabel('X (m)')
+plt.ylabel('$\Psi$ ($m^2/s$)')
+plt.title("DIFUSION, t = 1s")
 
 plt.savefig("difusion.png")
